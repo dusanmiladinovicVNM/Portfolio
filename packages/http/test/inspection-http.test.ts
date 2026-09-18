66a7578ab04a0823b9e841d1b000f0e7ccf07d84
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-09-19 01:09:42 +0200

    test: wire asset repository into HTTP harness

--- packages/http/test/inspection-http.test.ts
+++ packages/http/test/inspection-http.test.ts
@@ -35,6 +35,7 @@ import {
   type UnitId,
 } from '@portfolio/domain';
 import { createPortfolioHttpHandler } from '../src/index.js';
+import { InMemoryAssetRepository } from './asset-test-deps.js';
 import {
   FixedClock,
   InMemoryDocumentRepository,
