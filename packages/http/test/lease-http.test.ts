837cbaa355e9bb055f9fdf4e6b0cfbb9c8b3ff58
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-09-19 22:26:32 +0200

    test(http): wire Maintenance into lease-http root

--- packages/http/test/lease-http.test.ts
+++ packages/http/test/lease-http.test.ts
@@ -52,6 +52,7 @@ import {
 } from './inspection-test-deps.js';
 import { InMemoryImprovementRepository } from './improvement-test-deps.js';
 import { InMemoryCostRepository } from './cost-test-deps.js';
+import { InMemoryMaintenanceRepository } from './maintenance-test-deps.js';
 
 const adminIdentity: VerifiedIdentity = {
   provider: 'supabase',
@@ -454,6 +455,7 @@ function buildHandler() {
     assetServiceRepository: new InMemoryAssetServiceRepository(),
     improvementRepository: new InMemoryImprovementRepository(),
     costRepository: new InMemoryCostRepository(),
+    maintenanceRepository: new InMemoryMaintenanceRepository(),
     portfolioRepository: new EmptyPortfolioRepository(),
     partyRepository,
     ownershipRepository: new EmptyOwnershipRepository(),
