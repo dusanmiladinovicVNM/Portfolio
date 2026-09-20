a513bba1f5782637b84f28163f44dbc94bc97b44
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-09-20 08:30:09 +0200

    test(http): wire Meter repository root

--- packages/http/test/tenancy-http.test.ts
+++ packages/http/test/tenancy-http.test.ts
@@ -47,6 +47,7 @@ import { InMemoryImprovementRepository } from './improvement-test-deps.js';
 import { InMemoryCostRepository } from './cost-test-deps.js';
 import { InMemoryMaintenanceRepository } from './maintenance-test-deps.js';
 import { InMemoryAccessItemRepository } from './access-item-test-deps.js';
+import { InMemoryMeterRepository } from './meter-test-deps.js';
 
 const adminIdentity: VerifiedIdentity = {
   provider: 'supabase',
@@ -283,6 +284,7 @@ function buildHandler() {
 
   const handler = createPortfolioHttpHandler({
     accessItemRepository,
+    meterRepository: new InMemoryMeterRepository(),
     assetRepository: new InMemoryAssetRepository(),
     assetInventoryRepository: new InMemoryAssetInventoryRepository(),
     assetServiceRepository: new InMemoryAssetServiceRepository(),
