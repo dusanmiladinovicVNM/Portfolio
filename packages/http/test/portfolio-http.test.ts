ad1261c5950c7b6fd7964647bc0e407e625ed31b
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-09-21 11:33:09 +0200

    test(http): wire reporting repository root

--- packages/http/test/portfolio-http.test.ts
+++ packages/http/test/portfolio-http.test.ts
@@ -51,6 +51,7 @@ import { InMemoryMaintenanceRepository } from './maintenance-test-deps.js';
 import { InMemoryAccessItemRepository } from './access-item-test-deps.js';
 import { InMemoryMeterRepository } from './meter-test-deps.js';
 import { InMemoryUnitTimelineRepository } from './unit-timeline-test-deps.js';
+import { InMemoryReportingRepository } from './reporting-test-deps.js';
 
 const adminIdentity: VerifiedIdentity = {
   provider: 'supabase',
@@ -2289,6 +2290,7 @@ describe('Portfolio HTTP boundary', () => {
         accessItemRepository: new InMemoryAccessItemRepository(),
     meterRepository: new InMemoryMeterRepository(),
     unitTimelineRepository: new InMemoryUnitTimelineRepository(),
+    reportingRepository: new InMemoryReportingRepository(),
         assetRepository: new InMemoryAssetRepository(),
     assetInventoryRepository: new InMemoryAssetInventoryRepository(),
         assetServiceRepository: new InMemoryAssetServiceRepository(),
