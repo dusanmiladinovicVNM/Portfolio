66d0eaedc51e5535cb4f8b3e1f5f2504beb3caba
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-09-26 21:44:40 +0200

    Export Luzerner lease form application

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -20,6 +20,7 @@ export * from './tenancy/tenancy-queries.js';
 export * from './contracts/lease-repository.js';
 export * from './contracts/lease-commands.js';
 export * from './contracts/lease-queries.js';
+export * from './contracts/luzerner-lease-form.js';
 export * from './shared/clock.js';
 export * from './documents/document-binary-policy.js';
 export * from './documents/file-storage-port.js';
